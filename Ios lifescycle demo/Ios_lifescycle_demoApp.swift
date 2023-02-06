14bbe20e75b824c5102cca9306dae6942d15cf56
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-02-06 10:20:21 +0530

    Initial Commit

---
+++ Ios lifescycle demo/Ios_lifescycle_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_lifescycle_demoApp.swift
+//  Ios lifescycle demo
+//
+//  Created by HEMANTH on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Ios_lifescycle_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
